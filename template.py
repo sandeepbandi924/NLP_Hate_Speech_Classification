b0f286d1da130209667c76551cba13930e1f2cc4
sandeepbandi924 <bandisandeep1374@gmail.com>
2024-12-16 19:20:32 +0530

    project template

--- template.py
+++ template.py
@@ -13,8 +13,8 @@ list_of_files = [
     f'{project_name}/components/model_trainer.py',
     f'{project_name}/components/model_evaluation.py',
     f'{project_name}/components/model_pusher.py',
-    f'{project_name}/configuration/gcloud_syncer.py',
     f'{project_name}/configuration/__init__.py',
+    f'{project_name}/configuration/gcloud_syncer.py',
     f'{project_name}/constants/__init__.py',
     f'{project_name}/entity/__init__.py',
     f'{project_name}/entity/config_entity.py',
@@ -22,7 +22,7 @@ list_of_files = [
     f'{project_name}/exception/__init__.py',
     f'{project_name}/logger/__init__.py',
     f'{project_name}/pipeline/__init__.py',
-    f'{project_name}/pipeline/train_pipeline.py'
+    f'{project_name}/pipeline/train_pipeline.py',
     f'{project_name}/pipeline/prediction_pipeline.py',
     f'{project_name}/ml/__init__.py',
     f'{project_name}/ml/model.py',
